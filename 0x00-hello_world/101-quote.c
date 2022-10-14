282be18233974a5cdce077be2b9e6a9351ac89bf
sti-x <itibicharles@gmail.com>
2022-10-14 01:33:14 -0700

    extra

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * outputted to standard error
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+  write(2, "\"and that piece of art is useful\" 
+- Dora Korpar, 2015-10-19\n", 59);
+  return (1);
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * outputted to standard error
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+  write(2, "\"and that piece of art is useful\" 
+- Dora Korpar, 2015-10-19\n", 59);
+  return (1);
+}
