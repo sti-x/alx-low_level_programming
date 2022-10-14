d4efe12a9cae4229ebea8ac6e96c12a0d2718132
sti-x <itibicharles@gmail.com>
2022-10-14 01:42:35 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,3 @@
-/*
- * File: 4-puts.c
- * Auth: Brennan D Baraban <375@holbertonschool.com>
- */
-
 #include <stdio.h>
 
 /**
@@ -11,9 +6,11 @@
  *
  * Return: Always 0.
  */
+
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 }
